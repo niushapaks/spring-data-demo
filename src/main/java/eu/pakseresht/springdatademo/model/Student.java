358366a848449763bd79facd6167c3e600beaace
Niusha Pakseresht <niusha.pakseresht@weareadaptive.com>
2021-11-23 16:29:16 -0500

    typo in comments

--- src/main/java/eu/pakseresht/springdatademo/model/Student.java
+++ src/main/java/eu/pakseresht/springdatademo/model/Student.java
@@ -19,8 +19,8 @@ public class Student {
     }
 
     /**
-     * I chose Cascade ALL because I don't want to keep registrations linked to my student, when I create a student
-     * and when I delete it (to avoid orphan registration records).
+     * I chose Cascade ALL because I want to record registrations linked to my student when I create it,
+     * and want to delete their registrations when I delete it (to avoid orphan registration records).
      **/
     @OneToMany(mappedBy = "student", cascade = {CascadeType.ALL})
     private List<Registration> coursesRegistered= new ArrayList<>();
